b83701448affad54021bb958062a8fef291d15fa
Harshad Chand <shivatahal@gmail.com>
2021-11-25 06:35:22 +1200

    First commit

---
+++ Velocity-main/lib/screens/home/components/special_offers.dart
@@ -0,0 +1,118 @@
+import 'package:ecommerece_velocity_app/screens/category/category_list_page.dart';
+import 'package:flutter/material.dart';
+
+import '../../../size_config.dart';
+import 'section_title.dart';
+
+class SpecialOffers extends StatelessWidget {
+  const SpecialOffers({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SectionTitle(
+            title: "Special for you",
+            press: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => CategorySection()));
+            },
+          ),
+        ),
+        SizedBox(height: getProportionateScreenWidth(20)),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SpecialOfferCard(
+                image: "assets/images/tshirt.png",
+                category: "Shirt Special",
+                press: () {},
+              ),
+              SpecialOfferCard(
+                image: "assets/images/glap.png",
+                category: "Sports wear",
+                press: () {},
+              ),
+              SizedBox(width: getProportionateScreenWidth(20)),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SpecialOfferCard extends StatelessWidget {
+  const SpecialOfferCard({
+    Key? key,
+    required this.category,
+    required this.image,
+    required this.press,
+  }) : super(key: key);
+
+  final String category, image;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: getProportionateScreenWidth(20)),
+      child: GestureDetector(
+        onTap: press,
+        child: SizedBox(
+          width: getProportionateScreenWidth(242),
+          height: getProportionateScreenWidth(100),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Stack(
+              children: [
+                Image.asset(
+                  image,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xFF343434).withOpacity(0.4),
+                        Color(0xFF343434).withOpacity(0.15),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(15.0),
+                    vertical: getProportionateScreenWidth(10),
+                  ),
+                  child: Text.rich(
+                    TextSpan(
+                      style: TextStyle(color: Colors.white),
+                      children: [
+                        TextSpan(
+                          text: "$category\n",
+                          style: TextStyle(
+                            fontSize: getProportionateScreenWidth(18),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
